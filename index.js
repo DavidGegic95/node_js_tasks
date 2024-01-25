7ad761594f2c8ff144d9a70f1b3541cd90d61f6c
David <davidgegic95@gmail.com>
2024-01-25 20:26:59 +0100

    task 2 done

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
-function getRandomNumber() {
-    let random = Math.floor(Math.random() * 100)
-    return `Random number is ${random}`
-};
+const os = require("os")
+const childProcess = require('child_process')
+const fs = require('fs')
 
-if (require.main === module) {
-    console.log(getRandomNumber());
+const execProcess = (command) => {
+    childProcess.exec(command, (error, stdout, stderr) => {
+        console.clear()
+        console.log(`${stdout}\r`);
+        if (error !== null) {
+            process.stdout.write(`error: ${error}\r`);
+        }
+    });
 }
 
-module.exports = getRandomNumber;
+
+const saveActivityMonitor = (command) => {
+    childProcess.exec(command, (error, stdout, stderr) => {
+        const currentTime = Date.now()
+        const logMessage = `${currentTime} : ${stdout}`;
+
+        fs.appendFile("activityMonitor.log", logMessage, (err) => {
+            if (err) throw err;
+        });
+        if (error !== null) {
+            console.error(`Error: ${error}`);
+        }
+    });
+}
+
+function runActivityMonitor() {
+    const platform = os.platform()
+    const commandWindows = "Get-Process | Sort-Object CPU -Descending | Select-Object -Property Name, CPU, WorkingSet -First 1 | ForEach-Object { $_.Name + ' ' + $_.CPU + ' ' + $_.WorkingSet }"
+    const commandUnix = 'ps -A -o %cpu,%mem,comm | sort -nr | head -n 1'
+
+    setInterval(() => {
+        if (platform === 'win32') {
+            execProcess(commandWindows)
+        } else {
+            execProcess(commandUnix)
+        }
+    }, 100)
+
+    setInterval(() => {
+        if (platform === 'win32') {
+            saveActivityMonitor(commandWindows)
+        } else {
+            saveActivityMonitor(commandUnix)
+
+        }
+
+    }, 60 * 1000)
+
+}
+
+runActivityMonitor()
+
+
+module.exports = runActivityMonitor
